cfe80a8d27e9b5f1d2db97b46c848e45ab50cc4e
dlzagu <eodnsdlekd@naver.com>
2022-04-08 00:35:14 +0900

    20220408:React Router 2 : Link, Switch, history 기능"

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
 import './App.css';
 import Product from './data';
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail';
+
 
 function App() {
 
@@ -13,12 +16,12 @@ function App() {
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">zhuro</Navbar.Brand>
+          <Navbar.Brand><Link to="/">zhuro</Link></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+              <Nav.Link ><Link to="/detail">Detail</Link></Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -30,32 +33,39 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className='jumbotron'>
-        <h1>20% SeasonOff</h1>
-        <p>asdasd</p>
-        <button type="button" class="btn btn-primary">Primary</button>
-      </div>
-      <div className='container'>
-        <div className='row'>
-          {
-            상품.map((a) => {
-              return(
-                <상품진열 주소={a.이미지주소} 제목={a.title} 내용={a.content} 가격={a.price}></상품진열>
-              )
-            })
-          }
-         
-        </div>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <div className='jumbotron'>
+            <h1>20% SeasonOff</h1>
+            <p>asdasd</p>
+            <button type="button" class="btn btn-primary">Primary</button>
+          </div>
+          <div className='container'>
+            <div className='row'>
+              {
+                상품.map((a, i) => {
+                  return (
+                    <상품진열 상품={상품[i]}></상품진열>
+                  )
+                })
+              }
+
+            </div>
+          </div>
+        </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+      </Switch>
     </div>
   );
 }
 function 상품진열(props) {
   return (
     <div className='col-md-4'>
-      <img src={props.주소} width="100%" />
-      <h4>{props.제목}</h4>
-      <p>{props.내용} & {props.가격}</p>
+      <img src={props.상품.이미지주소} width="100%" />
+      <h4>{props.상품.title}</h4>
+      <p>{props.상품.content} & {props.상품.price}</p>
     </div>
   )
 }
