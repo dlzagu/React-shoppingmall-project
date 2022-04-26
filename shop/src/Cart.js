7cf5f1fa8ea3af32d75be65864bcf1fa86dfe1ed
dlzagu <eodnsdlekd@naver.com>
2022-04-26 21:28:10 +0900

    세계최고로 쉬운 Redux 2 : reducer/dispatch로 데이터 수정하는 법

--- shop/src/Cart.js
+++ shop/src/Cart.js
@@ -5,24 +5,32 @@ function Cart(props) {
   return (
     <div>
       <Table responsive>
+      <tbody>
         <tr>
           <th>#</th>
           <th>상품명</th>
           <th>수량</th>
           <th>변경</th>
         </tr>
+        </tbody>
+        <tbody>
         {
-          props.state.map((a,i)=>{
-          return(
-            <tr>
-          <td>{i+1}</td>
-          <td>{a.name}</td>
-          <td>{a.quan}</td>
-          <td>{a.name}</td>
-        </tr>
-          )
+          props.state.map((a, i) => {
+            return (
+              <tr key = {i}>
+                <td>{a.id}</td>
+                <td>{a.name}</td>
+                <td>{a.quan}</td>
+                <td>
+                <button onClick ={()=>{props.dispatch({type:'수량증가'})}}>+</button>
+                <button onClick ={()=>{props.dispatch({type:'수량감소'})}}>-</button>
+                </td>
+                
+              </tr>
+            )
           })
         }
+        </tbody>
       </Table>
     </div>
   )
