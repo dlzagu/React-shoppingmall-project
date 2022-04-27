e3815726c6ac9255d72f2dde69e8ae5138bdc46a
dlzagu <eodnsdlekd@naver.com>
2022-04-27 22:46:30 +0900

    세계최고로 쉬운 Redux 5 : useSelector, useDispatch

--- shop/src/Cart.js
+++ shop/src/Cart.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Table } from "react-bootstrap"
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
+
 function Cart(props) {
+
+  let state = useSelector((state)=> state);
+  let dispatch = useDispatch();
+
   return (
     <div>
       <Table responsive>
-      <tbody>
+      <tbody> 
         <tr>
           <th>#</th>
           <th>상품명</th>
@@ -15,15 +20,15 @@ function Cart(props) {
         </tbody>
         <tbody>
         {
-          props.state.map((a, i) => {
+          state.reducer.map((a, i) => {
             return (
               <tr key = {i}>
                 <td>{a.id}</td>
                 <td>{a.name}</td>
                 <td>{a.quan}</td>
                 <td>
-                <button onClick ={()=>{props.dispatch({type:'수량증가'})}}>+</button>
-                <button onClick ={()=>{props.dispatch({type:'수량감소'})}}>-</button>
+                <button onClick ={()=>{dispatch({type:'수량증가'})}}>+</button>
+                <button onClick ={()=>{dispatch({type:'수량감소'})}}>-</button>
                 </td>
                 
               </tr>
@@ -36,7 +41,7 @@ function Cart(props) {
         props.alert열렸니?
         <div className="my-alert">
           <p>지금 구매하시면 신규할인 20%</p>
-          <button onClick={()=>{props.dispatch({type:'닫기'})}}>닫기</button>
+          <button onClick={()=>{dispatch({type:'닫기'})}}>닫기</button>
         </div>
         :null
       }
@@ -51,4 +56,4 @@ function state를props화(state) {
 }
 
 
-export default connect(state를props화)(Cart);
+export default Cart;
