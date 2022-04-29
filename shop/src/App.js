4bf967effd497d5f4de55d79de6bb5908c5c92c2
dlzagu <eodnsdlekd@naver.com>
2022-04-29 23:42:17 +0900

    20220429:최근본상품목록 만들기

--- shop/src/App.js
+++ shop/src/App.js
@@ -73,7 +73,9 @@ function App() {
                 })
                 .catch(() => console.log("실패"));
             }}>더보기</button>
+            <RecentlyView 상품={상품}/>
           </div>
+          
         </Route>
         <Route path="/detail/:id">
           <Detail 상품={상품} 재고={재고} 재고변경={재고변경} />
@@ -85,15 +87,61 @@ function App() {
     </div>
   );
 }
+
 function Card(props) {
+  function 이미지누르면 (){
+    history.push(history.push(`/detail/${props.상품.id}`));
+    if(최근본상품ID가져오기!==null){
+      let newID = JSON.parse(최근본상품ID가져오기);
+      if (!newID.includes(props.상품.id)){
+        newID.push(props.상품.id);
+        localStorage.setItem("id",JSON.stringify(newID));
+      }
+      
+    }else{
+      localStorage.setItem("id",JSON.stringify([props.상품.id]));
+    }
+    
+
+  }
   let history = useHistory();
+  let 최근본상품ID가져오기 = localStorage.getItem("id");
+  
   return (
-    <div className='col-md-4' onClick={()=>{history.push(history.push(`/detail/${props.상품.id}`))}}>
+    <div className='col-md-4' onClick={이미지누르면}>
       <img src={props.상품.이미지주소} width="100%" />
       <h4>{props.상품.title}</h4>
       <p>{props.상품.content} & {props.상품.price}</p>
     </div>
   )
+}
+function RecentlyView(props){
+  let recnetlyList = JSON.parse(localStorage.getItem("id"));
+  let history = useHistory();
+ return(
+  
+    <div className='row'>
+    <h4 className='mt-4'>최근 본 상품</h4>
+    
+  
+  { 
+  recnetlyList!==null
+  ?
+    recnetlyList.map(a=>{
+      let 찾은이미지 = props.상품.find((상품)=>상품.id===a);
+      return(
+        <div className='col-md-4' onClick={()=>{history.push(history.push(`/detail/${찾은이미지.id}`));}}>
+        <img src={찾은이미지.이미지주소} width="100%" />
+        </div>
+      )
+    })
+    :
+    null
+  }
+    
+</div>
+
+ )
 }
 
 export default App;
--- shop/src/Cart.js
+++ shop/src/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Table } from "react-bootstrap"
 import { connect, useDispatch, useSelector } from "react-redux";
 
-function Cart(props) {
+function Cart() {
 
   let state = useSelector((state)=> state);
   let dispatch = useDispatch();
@@ -41,7 +41,7 @@ function Cart(props) {
         </tbody>
       </Table>
       {
-        props.alert열렸니?
+        state.reducer2?
         <div className="my-alert">
           <p>지금 구매하시면 신규할인 20%</p>
           <button onClick={()=>{dispatch({type:'닫기'})}}>닫기</button>
